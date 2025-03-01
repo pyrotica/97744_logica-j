abfc798153c02f1f43a7d63159b24c807db45972
Miguel <perfectact.2023@gmail.com>
2025-03-01 19:39:49 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,11 @@
+def BubbleSport(lista):
+    tamanho = len(lista)
+    for i in range(tamanho):
+       for j in range(0, tamanho - i - 1):
+         if lista[j]>lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[1]
+    return lista
+
+lista = [2,12,3,0,8,90,69]
+lista_ordenada = BubbleSport(lista)
+print("Lista ordenada:", lista_ordenada)
